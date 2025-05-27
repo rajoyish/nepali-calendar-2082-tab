c229c421dfba85db3a4fade714e4b6f8aae6dd45
Rajesh Budhathoki <rajoyish@gmail.com>
2025-05-27 15:06:24 +0545

    Init changing background images

---
+++ src/bgImages.js
@@ -0,0 +1,8 @@
+import bg1 from "../assets/bg (1).jpeg";
+import bg2 from "../assets/bg (2).jpeg";
+import bg3 from "../assets/bg (3).jpeg";
+import bg4 from "../assets/bg (4).jpeg";
+import bg5 from "../assets/bg (5).jpeg";
+import bg6 from "../assets/bg (6).jpeg";
+
+export const bgImages = [bg1, bg2, bg3, bg4, bg5, bg6];

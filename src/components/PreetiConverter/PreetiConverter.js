22e8f100e7ad6e1ad1c7c89dc4cfdf6384baa4bc
Rajesh Maria <rajoyish@gmail.com>
2026-05-01 11:10:09 +0545

    refactor(PreetiConverter): update copy button position and icons

--- src/components/PreetiConverter/PreetiConverter.js
+++ src/components/PreetiConverter/PreetiConverter.js
@@ -40,7 +40,8 @@ export const initPreetiConverter = (containerId) => {
           <textarea id="unicodeOutput" class="preeti-app__textarea preeti-app__textarea--target" placeholder="यहाँ यूनिकोड नेपालीमा रुपान्तरण हुनेछ ।" readonly></textarea>
           <div class="preeti-app__copy-bar">
             <button id="copyBtn" type="button" class="preeti-app__copy-btn">
-              <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="preeti-app__copy-icon"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z" /></svg>
+              <i class="bi bi-clipboard copy-icon-default"></i>
+              <i class="bi bi-clipboard-check copy-icon-active"></i>
             </button>
           </div>
         </div>
@@ -94,9 +95,11 @@ export const initPreetiConverter = (containerId) => {
 
   const showAlert = () => {
     dom.alert.classList.add("preeti-app__alert--visible");
+    dom.copyBtn.classList.add("is-copied");
     if (alertTimeout) clearTimeout(alertTimeout);
     alertTimeout = setTimeout(() => {
       dom.alert.classList.remove("preeti-app__alert--visible");
+      dom.copyBtn.classList.remove("is-copied");
     }, 3000);
   };
 
